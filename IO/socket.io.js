ec01269cff9f4e88fb0d7719a0954bb793330f0b
Kedar Devasthali <67545340+kedar-devs@users.noreply.github.com>
2023-06-05 12:13:53 +0530

    Feat:Add more scripts to socket.io

--- IO/socket.io.js
+++ IO/socket.io.js
@@ -1,4 +1,6 @@
 const io=require('socket.io')(3000)
 io.on("connection",socket=>{
-    socket.emit("chat-message","Hello User")
+    socket.on("send-chat-message",(mssg)=>{
+        socket.broadcast.emit("chat-message",mssg)
+    })
 })
--- server.js
+++ server.js
@@ -2,6 +2,13 @@ const app=require('./app')
 const {DBConnect}=require('./DB/mongoDbConnect')
 const port=process.env.PORT || 5000
 const connect=DBConnect()
+const io=require('socket.io')(3080)
+io.on("connection",socket=>{
+    console.log("connected")
+    socket.on("send-chat-message",(mssg)=>{
+        socket.broadcast.emit("chat-message",mssg)
+    })
+})
 if(connect){
     app.listen(port,()=>{
         console.log(`Server is up on port:${port}`)
